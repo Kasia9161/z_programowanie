ad8c0bb8afdd7d45d14681d0b88409e4ce5570cb
Katarzyna Adamczyk <57094940+Kasia9161@users.noreply.github.com>
2022-10-11 23:02:30 +0200

    Add files via upload

---
+++ zad_2a.py
@@ -0,0 +1,7 @@
+#zadanie 2a
+x = ['Anna', 'Jan', 'Karol', 'Ola', 'Wiktor']
+
+def imiona(names):
+    for name in names:
+        print(name)
+imiona(x)
---
+++ zad_2c.py
@@ -0,0 +1,8 @@
+#zadanie 2c
+
+def parzyste(p):
+    for i in p:
+        if i % 2 == 0:
+            print(i)
+
+parzyste(range(10))
